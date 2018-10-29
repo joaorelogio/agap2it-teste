8ca333ac44357b2bc09035cbb1dd25401d15783f
João Paulo Relógio <joaoprelogio@gmail.com>
2018-10-29 16:51:13 +0000

    Create component empty

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { TopbarComponent } from './components/topbar/topbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ContentComponent } from './components/content/content.component';
+import { EmptyComponent } from './components/empty/empty.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopbarComponent,
     SidebarComponent,
-    ContentComponent
+    ContentComponent,
+    EmptyComponent
   ],
   imports: [
     BrowserModule,
